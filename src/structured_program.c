e65acf5116c9d7fe7c5f311a5a7d7ab73439bbea
NB11B <124950290+NB11B@users.noreply.github.com>
2026-07-16 14:50:21 -0400

    Implement metric and projective structured bytecode operations

--- src/structured_program.c
+++ src/structured_program.c
@@ -17,6 +17,20 @@ geo_struct_value_t geo_struct_value_from_cl20(geo_cl20_t value) {
     return result;
 }
 
+geo_struct_value_t geo_struct_value_from_scalar(geo_real_t value) {
+    geo_struct_value_t result;
+    result.kind = (uint8_t)GEO_STRUCT_VALUE_SCALAR;
+    result.as.scalar = value;
+    return result;
+}
+
+geo_struct_value_t geo_struct_value_from_scaled_cl20(geo_scaled_cl20_t value) {
+    geo_struct_value_t result;
+    result.kind = (uint8_t)GEO_STRUCT_VALUE_SCALED_CL20;
+    result.as.scaled_cl20 = value;
+    return result;
+}
+
 geo_status_t geo_struct_program_execute(
     const geo_struct_program_t *program,
     geo_struct_value_t *registers,
@@ -55,92 +69,178 @@ geo_status_t geo_struct_program_execute(
 
             case GEO_STRUCT_OP_CL20_NEGATE:
                 status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_CL20);
-                if (status != GEO_STATUS_OK) {
-                    return status;
-                }
+                if (status != GEO_STATUS_OK) return status;
                 result = geo_struct_value_from_cl20(geo_cl20_neg(left.as.cl20));
                 break;
 
             case GEO_STRUCT_OP_UNIPOTENT_ENCODE:
                 status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_CL20);
-                if (status != GEO_STATUS_OK) {
-                    return status;
-                }
+                if (status != GEO_STATUS_OK) return status;
                 result.kind = (uint8_t)GEO_STRUCT_VALUE_UNIPOTENT;
                 result.as.unipotent = geo_unipotent_from_cl20(left.as.cl20);
                 break;
 
             case GEO_STRUCT_OP_UNIPOTENT_COMPOSE:
                 status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_UNIPOTENT);
-                if (status != GEO_STATUS_OK) {
-                    return status;
-                }
+                if (status != GEO_STATUS_OK) return status;
                 status = geo_struct_require_kind(&right, GEO_STRUCT_VALUE_UNIPOTENT);
-                if (status != GEO_STATUS_OK) {
-                    return status;
-                }
+                if (status != GEO_STATUS_OK) return status;
                 result.kind = (uint8_t)GEO_STRUCT_VALUE_UNIPOTENT;
-                result.as.unipotent = geo_unipotent_mul(
-                    left.as.unipotent,
-                    right.as.unipotent
-                );
+                result.as.unipotent = geo_unipotent_mul(left.as.unipotent, right.as.unipotent);
                 break;
 
             case GEO_STRUCT_OP_UNIPOTENT_EXTRACT:
                 status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_UNIPOTENT);
-                if (status != GEO_STATUS_OK) {
-                    return status;
-                }
-                result = geo_struct_value_from_cl20(
-                    geo_unipotent_extract(left.as.unipotent)
-                );
+                if (status != GEO_STATUS_OK) return status;
+                result = geo_struct_value_from_cl20(geo_unipotent_extract(left.as.unipotent));
                 break;
 
             case GEO_STRUCT_OP_ORDERED_PRODUCTS:
                 status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_CL20);
-                if (status != GEO_STATUS_OK) {
-                    return status;
-                }
+                if (status != GEO_STATUS_OK) return status;
                 status = geo_struct_require_kind(&right, GEO_STRUCT_VALUE_CL20);
-                if (status != GEO_STATUS_OK) {
-                    return status;
-                }
+                if (status != GEO_STATUS_OK) return status;
                 result.kind = (uint8_t)GEO_STRUCT_VALUE_ORDERED_PAIR;
                 result.as.ordered = geo_ordered_products(left.as.cl20, right.as.cl20);
                 break;
 
             case GEO_STRUCT_OP_HADAMARD_EXACT:
                 status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_ORDERED_PAIR);
-                if (status != GEO_STATUS_OK) {
-                    return status;
-                }
+                if (status != GEO_STATUS_OK) return status;
                 result.kind = (uint8_t)GEO_STRUCT_VALUE_HADAMARD_PAIR;
                 result.as.hadamard = geo_hadamard_mix_exact(left.as.ordered);
                 break;
 
             case GEO_STRUCT_OP_HADAMARD_PROJECTIVE:
                 status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_ORDERED_PAIR);
-                if (status != GEO_STATUS_OK) {
-                    return status;
-                }
+                if (status != GEO_STATUS_OK) return status;
                 result.kind = (uint8_t)GEO_STRUCT_VALUE_HADAMARD_PAIR;
                 result.as.hadamard = geo_hadamard_mix_projective(left.as.ordered);
                 break;
 
             case GEO_STRUCT_OP_SELECT_SYMMETRIC:
                 status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_HADAMARD_PAIR);
-                if (status != GEO_STATUS_OK) {
-                    return status;
-                }
-                result = geo_struct_value_from_cl20(left.as.hadamard.symmetric.represented);
+                if (status != GEO_STATUS_OK) return status;
+                result = geo_struct_value_from_scaled_cl20(left.as.hadamard.symmetric);
                 break;
 
             case GEO_STRUCT_OP_SELECT_ANTISYMMETRIC:
                 status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_HADAMARD_PAIR);
-                if (status != GEO_STATUS_OK) {
-                    return status;
+                if (status != GEO_STATUS_OK) return status;
+                result = geo_struct_value_from_scaled_cl20(left.as.hadamard.antisymmetric);
+                break;
+
+            case GEO_STRUCT_OP_CL20_SUBTRACT:
+                status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_CL20);
+                if (status != GEO_STATUS_OK) return status;
+                status = geo_struct_require_kind(&right, GEO_STRUCT_VALUE_CL20);
+                if (status != GEO_STATUS_OK) return status;
+                result = geo_struct_value_from_cl20(geo_cl20_sub(left.as.cl20, right.as.cl20));
+                break;
+
+            case GEO_STRUCT_OP_CL20_SCALE:
+                status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_CL20);
+                if (status != GEO_STATUS_OK) return status;
+                status = geo_struct_require_kind(&right, GEO_STRUCT_VALUE_SCALAR);
+                if (status != GEO_STATUS_OK) return status;
+                result = geo_struct_value_from_cl20(geo_cl20_scale(left.as.cl20, right.as.scalar));
+                break;
+
+            case GEO_STRUCT_OP_SCALAR_EXTRACT:
+                status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_CL20);
+                if (status != GEO_STATUS_OK) return status;
+                result = geo_struct_value_from_scalar(left.as.cl20.scalar);
+                break;
+
+            case GEO_STRUCT_OP_SCALAR_MULTIPLY:
+                status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_SCALAR);
+                if (status != GEO_STATUS_OK) return status;
+                status = geo_struct_require_kind(&right, GEO_STRUCT_VALUE_SCALAR);
+                if (status != GEO_STATUS_OK) return status;
+                result = geo_struct_value_from_scalar(left.as.scalar * right.as.scalar);
+                break;
+
+            case GEO_STRUCT_OP_VECTOR_NORM_SQUARED:
+                status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_CL20);
+                if (status != GEO_STATUS_OK) return status;
+                result = geo_struct_value_from_scalar(geo_cl20_vector_norm_squared(left.as.cl20));
+                break;
+
+            case GEO_STRUCT_OP_DISTANCE_SQUARED:
+                status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_CL20);
+                if (status != GEO_STATUS_OK) return status;
+                status = geo_struct_require_kind(&right, GEO_STRUCT_VALUE_CL20);
+                if (status != GEO_STATUS_OK) return status;
+                result = geo_struct_value_from_scalar(
+                    geo_cl20_vector_norm_squared(geo_cl20_sub(left.as.cl20, right.as.cl20))
+                );
+                break;
+
+            case GEO_STRUCT_OP_PROJECTION_NUMERATOR:
+                status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_CL20);
+                if (status != GEO_STATUS_OK) return status;
+                status = geo_struct_require_kind(&right, GEO_STRUCT_VALUE_CL20);
+                if (status != GEO_STATUS_OK) return status;
+                result = geo_struct_value_from_scaled_cl20((geo_scaled_cl20_t){
+                    geo_cl20_scale(right.as.cl20, geo_cl20_vector_dot(left.as.cl20, right.as.cl20)),
+                    {1, 1}
+                });
+                break;
+
+            case GEO_STRUCT_OP_REJECTION_NUMERATOR:
+                status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_CL20);
+                if (status != GEO_STATUS_OK) return status;
+                status = geo_struct_require_kind(&right, GEO_STRUCT_VALUE_CL20);
+                if (status != GEO_STATUS_OK) return status;
+                result = geo_struct_value_from_scaled_cl20((geo_scaled_cl20_t){
+                    geo_cl20_sub(
+                        geo_cl20_scale(left.as.cl20, geo_cl20_vector_norm_squared(right.as.cl20)),
+                        geo_cl20_scale(right.as.cl20, geo_cl20_vector_dot(left.as.cl20, right.as.cl20))
+                    ),
+                    {1, 1}
+                });
+                break;
+
+            case GEO_STRUCT_OP_REFLECTION_NUMERATOR:
+                status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_CL20);
+                if (status != GEO_STATUS_OK) return status;
+                status = geo_struct_require_kind(&right, GEO_STRUCT_VALUE_CL20);
+                if (status != GEO_STATUS_OK) return status;
+                result = geo_struct_value_from_scaled_cl20((geo_scaled_cl20_t){
+                    geo_cl20_sub(
+                        geo_cl20_scale(left.as.cl20, geo_cl20_vector_norm_squared(right.as.cl20)),
+                        geo_cl20_scale(
+                            right.as.cl20,
+                            (geo_real_t)2 * geo_cl20_vector_dot(left.as.cl20, right.as.cl20)
+                        )
+                    ),
+                    {1, 1}
+                });
+                break;
+
+            case GEO_STRUCT_OP_VECTOR_INVERSE_PROJECTIVE:
+                status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_CL20);
+                if (status != GEO_STATUS_OK) return status;
+                result = geo_struct_value_from_scaled_cl20((geo_scaled_cl20_t){left.as.cl20, {1, 1}});
+                break;
+
+            case GEO_STRUCT_OP_ANGLE_COSINE_NUMERATOR:
+                status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_CL20);
+                if (status != GEO_STATUS_OK) return status;
+                status = geo_struct_require_kind(&right, GEO_STRUCT_VALUE_CL20);
+                if (status != GEO_STATUS_OK) return status;
+                result = geo_struct_value_from_scalar(geo_cl20_vector_dot(left.as.cl20, right.as.cl20));
+                break;
+
+            case GEO_STRUCT_OP_SCALED_CL20_NORMALIZE:
+                status = geo_struct_require_kind(&left, GEO_STRUCT_VALUE_SCALED_CL20);
+                if (status != GEO_STATUS_OK) return status;
+                {
+                    geo_scaled_cl20_t normalized;
+                    status = geo_scaled_cl20_normalize(&left.as.scaled_cl20, &normalized);
+                    if (status != GEO_STATUS_OK) return status;
+                    result = geo_struct_value_from_cl20(normalized.represented);
                 }
-                result = geo_struct_value_from_cl20(left.as.hadamard.antisymmetric.represented);
                 break;
 
             default:
@@ -157,12 +257,28 @@ geo_status_t geo_struct_read_cl20(
     const geo_struct_value_t *value,
     geo_cl20_t *output
 ) {
-    if (value == NULL || output == NULL) {
-        return GEO_STATUS_NULL_ARGUMENT;
-    }
-    if (value->kind != (uint8_t)GEO_STRUCT_VALUE_CL20) {
-        return GEO_STATUS_BAD_OPCODE;
-    }
+    if (value == NULL || output == NULL) return GEO_STATUS_NULL_ARGUMENT;
+    if (value->kind != (uint8_t)GEO_STRUCT_VALUE_CL20) return GEO_STATUS_BAD_OPCODE;
     *output = value->as.cl20;
+    return GEO_STATUS_OK;
+}
+
+geo_status_t geo_struct_read_scalar(
+    const geo_struct_value_t *value,
+    geo_real_t *output
+) {
+    if (value == NULL || output == NULL) return GEO_STATUS_NULL_ARGUMENT;
+    if (value->kind != (uint8_t)GEO_STRUCT_VALUE_SCALAR) return GEO_STATUS_BAD_OPCODE;
+    *output = value->as.scalar;
+    return GEO_STATUS_OK;
+}
+
+geo_status_t geo_struct_read_scaled_cl20(
+    const geo_struct_value_t *value,
+    geo_scaled_cl20_t *output
+) {
+    if (value == NULL || output == NULL) return GEO_STATUS_NULL_ARGUMENT;
+    if (value->kind != (uint8_t)GEO_STRUCT_VALUE_SCALED_CL20) return GEO_STATUS_BAD_OPCODE;
+    *output = value->as.scaled_cl20;
     return GEO_STATUS_OK;
 }
