6a520a410fc0ddb101169ca46a3ebdb1f980c209
NB11B <124950290+NB11B@users.noreply.github.com>
2026-07-17 14:27:45 -0400

    Add GEO float and fixed IMU filter interfaces

---
+++ benchmarks/esp32_imu_baseline/main/geo_filter.h
@@ -0,0 +1,40 @@
+#ifndef GEO_ESP32_GEO_FILTER_H
+#define GEO_ESP32_GEO_FILTER_H
+
+#include "benchmark_common.h"
+#include "geo/fixed.h"
+
+#include <stdint.h>
+
+typedef struct {
+    float qw;
+    float qx;
+    float qy;
+    float qz;
+    float integral_x;
+    float integral_y;
+    float integral_z;
+} geo_float_filter_t;
+
+typedef struct {
+    geo_fixed_t qw;
+    geo_fixed_t qx;
+    geo_fixed_t qy;
+    geo_fixed_t qz;
+    geo_fixed_t integral_x;
+    geo_fixed_t integral_y;
+    geo_fixed_t integral_z;
+    uint32_t arithmetic_failures;
+} geo_fixed_filter_t;
+
+void geo_float_filter_reset(void *state);
+benchmark_output_t geo_float_filter_step(void *state,
+                                         const imu_sample_t *sample,
+                                         float dt);
+
+void geo_fixed_filter_reset(void *state);
+benchmark_output_t geo_fixed_filter_step(void *state,
+                                         const imu_sample_t *sample,
+                                         float dt);
+
+#endif
