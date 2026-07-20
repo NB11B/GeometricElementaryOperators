6331b2401381ecf99edbccf273fe5fcc9b2dec56
NB11B <124950290+NB11B@users.noreply.github.com>
2026-07-20 00:07:26 -0400

    Make CUDA plan resident and separate reference training

--- include/geo/batch_gp_cuda.h
+++ include/geo/batch_gp_cuda.h
@@ -10,7 +10,7 @@
 extern "C" {
 #endif
 
-#define GEO_BATCH_GP_CUDA_ABI_VERSION 0x00010000u
+#define GEO_BATCH_GP_CUDA_ABI_VERSION 0x00010001u
 
 typedef enum {
     GEO_BATCH_GP_CUDA_OK = 0,
@@ -26,6 +26,7 @@ typedef struct {
     uint16_t term_count;
     uint8_t dimension;
     int8_t signature[GEO_OPERATOR_MAX_DIMENSION];
+    int8_t *device_signature;
     uint8_t *device_left_blade;
     uint8_t *device_right_blade;
     uint8_t *device_output_blade;
@@ -37,7 +38,9 @@ geo_batch_gp_cuda_status_t geo_batch_gp_cuda_plan_upload(geo_batch_gp_cuda_plan_
 void geo_batch_gp_cuda_plan_destroy(geo_batch_gp_cuda_plan_t *plan);
 geo_batch_gp_cuda_status_t geo_batch_gp_cuda_reference_forward_f64(const geo_batch_gp_cuda_plan_t *plan, const double *device_inputs, size_t batch_size, const double *device_parameter, int parameter_on_left, double *device_outputs, cudaStream_t stream);
 geo_batch_gp_cuda_status_t geo_batch_gp_cuda_planned_forward_f64(const geo_batch_gp_cuda_plan_t *plan, const double *device_inputs, size_t batch_size, const double *device_parameter, int parameter_on_left, double *device_outputs, cudaStream_t stream);
+geo_batch_gp_cuda_status_t geo_batch_gp_cuda_reference_parameter_vjp_f64(const geo_batch_gp_cuda_plan_t *plan, const double *device_inputs, const double *device_output_cotangents, size_t batch_size, int parameter_on_left, double *device_parameter_cotangent, cudaStream_t stream);
 geo_batch_gp_cuda_status_t geo_batch_gp_cuda_parameter_vjp_f64(const geo_batch_gp_cuda_plan_t *plan, const double *device_inputs, const double *device_output_cotangents, size_t batch_size, int parameter_on_left, double *device_parameter_cotangent, cudaStream_t stream);
+geo_batch_gp_cuda_status_t geo_batch_gp_cuda_reference_mse_sgd_step_f64(const geo_batch_gp_cuda_plan_t *plan, const double *device_inputs, const double *device_targets, size_t batch_size, double learning_rate, int parameter_on_left, double *device_parameter, double *device_residuals, double *device_gradient, double *device_loss, cudaStream_t stream);
 geo_batch_gp_cuda_status_t geo_batch_gp_cuda_mse_sgd_step_f64(const geo_batch_gp_cuda_plan_t *plan, const double *device_inputs, const double *device_targets, size_t batch_size, double learning_rate, int parameter_on_left, double *device_parameter, double *device_residuals, double *device_gradient, double *device_loss, cudaStream_t stream);
 const char *geo_batch_gp_cuda_status_string(geo_batch_gp_cuda_status_t status);
 
