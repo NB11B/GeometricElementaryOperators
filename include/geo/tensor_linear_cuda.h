b955c5563cb2765d3644bbcc296d444a659ea4da
NB11B <124950290+NB11B@users.noreply.github.com>
2026-07-25 12:45:47 -0400

    Add CUDA tensor linear API

---
+++ include/geo/tensor_linear_cuda.h
@@ -0,0 +1,33 @@
+#ifndef GEO_TENSOR_LINEAR_CUDA_H
+#define GEO_TENSOR_LINEAR_CUDA_H
+
+#include "geo/tensor_linear.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* CUDA stream is passed as an opaque cudaStream_t-compatible pointer. */
+geo_tensor_status geo_tensor_linear_cuda_forward(
+    const geo_real_t *x,
+    const geo_real_t *weight,
+    geo_real_t *y,
+    geo_tensor_linear_shape shape,
+    void *stream
+);
+
+geo_tensor_status geo_tensor_linear_cuda_vjp(
+    const geo_real_t *x,
+    const geo_real_t *weight,
+    const geo_real_t *grad_y,
+    geo_real_t *grad_x,
+    geo_real_t *grad_weight,
+    geo_tensor_linear_shape shape,
+    void *stream
+);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
