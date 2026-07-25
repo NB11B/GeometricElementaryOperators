89ab21229a7137a80886804e076e8842d2a368de
NB11B <124950290+NB11B@users.noreply.github.com>
2026-07-25 14:11:24 -0400

    Add GEO AdamW CUDA ABI

---
+++ include/geo/tensor_optimizer_cuda.h
@@ -0,0 +1,39 @@
+#ifndef GEO_TENSOR_OPTIMIZER_CUDA_H
+#define GEO_TENSOR_OPTIMIZER_CUDA_H
+
+#include "geo/tensor_optimizer.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+geo_tensor_status geo_tensor_grad_square_cuda_accumulate(
+    const float *gradient,
+    size_t count,
+    float *sum_square,
+    void *stream
+);
+
+geo_tensor_status geo_tensor_grad_clip_cuda_finalize(
+    const float *sum_square,
+    float max_grad_norm,
+    float *clip_scale,
+    void *stream
+);
+
+geo_tensor_status geo_tensor_adamw_cuda_step(
+    float *parameter,
+    const float *gradient,
+    float *first_moment,
+    float *second_moment,
+    size_t count,
+    const float *clip_scale,
+    geo_tensor_adamw_config config,
+    void *stream
+);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
